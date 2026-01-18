0c51ff87d3f3d14eb7cfabaf23f473f97a8614c9
Lucas-Go-Vel <lucas.gomez.velayos@alumnos.upm.es>
2026-01-18 12:33:28 +0100

    Apply modelica equations to `SinglePend.slx`
    Adjusted mass, length, and damping parameters in CalcLQRSingle.m and updated the Q matrix for LQR design. Modified Simulink models for the single pendulum system. Added the reference Modelica file (`penduloInvertido.mo`) implementing the inverted pendulum dynamics and used the equations inside of it for the `SinglePend.slx` Pendulum block.
    
    Also: IT WOOORKS!

--- CalcLQRSingle.m
+++ CalcLQRSingle.m
@@ -1,9 +1,9 @@
 clear,clc;
-m = 1; %Pendulum mass
-M = 5; %Cart mass
-L = 2; %Length to pendulum mass
+m = 0.3; %Pendulum mass
+M = 2.1; %Cart mass
+L = 0.5; %Length to pendulum mass
 g = 9.81; %m/s^2
-b = 0; %dampening
+b = 0.1; %dampening
 
 A = [0 1 0 0;...
     0 -b/M m*g/M 0;...
@@ -13,13 +13,18 @@ B = [0; 1/M; 0; 1/(M*L)];
 Bt = B';
 
 Q = [1,0,0,0;...
-    0,1,0,0;...
+    0,0,0,0;...
     0,0,1,0;...
-    0,0,0,1];
+    0,0,0,0];
 R = 1;
 R_1 = inv(R);
-[K,S,P] = lqr(A,B,Q,R); %K = R_1*Bt*S
+[K,S,P] = lqr(A,B,Q,R); %K = R^-1(B'S + Nt); N = 0
 
 if size(A,1) ~= size(S,1) || size(A,2) ~= size(S,2)
     disp('S and A should have the same size!!!');
 end
+
+% (u[1]/m - g*sin(u[3])*cos(u[3]) + l*power(u[2],2)*sin(u[3]))/(Mcart/m + power(sin(u[3]),2))
+% xDotDot = 
+% (F/m + g*sin(theta)*cos(theta) - L*ThetaDot^2*sin(theta)) / (M/m +
+% sin(theta)^2
